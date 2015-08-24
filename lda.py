4530b99e3858c79563fff1510eb8ad72f0de8e7c
Joshua Davis <josh314@gmail.com>
2015-08-24 15:39:39 -0400

    Added make-predictions.py which consumes a pickled model and outputs predictions on the test set as well as displayiong a few random test images and their predictions. Removed from lda.py the making of predictions and display of test images

--- lda.py
+++ lda.py
@@ -1,43 +1,32 @@
+import sys
+if len(sys.argv) != 2:
+    print "Usage: python k-fold-cv.py <pickled model output>"
+    raise SystemExit(1)
 import os
+import pickle
 import pandas as pd
 import numpy as np
-from sklearn import lda
-import matplotlib.pyplot as plt
+from sklearn import lda 
 
 os.chdir('/Users/josh/dev/kaggle/digit-recognizer')
 train = pd.read_csv('data/raw/train.csv',header=0)
 test = pd.read_csv('data/raw/test.csv',header=0)
 
-num_fit = 10000#len(train)
+num_fit = len(train)
 #Separate out label data and convert to numpy array
 train_predictor_data = train.iloc[ :num_fit, 1:].values
 train_target_data = train.iloc[ :num_fit, 0].values
 
 #Train dat sucker
-priors = np.array([.1]*10)
-clf = lda.LDA(priors=priors)
+#priors = np.array([.1]*10) #didn't help much. maybe hurt
+clf = lda.LDA()
 clf.fit(train_predictor_data, train_target_data)
-#How good was the fit on the training data?
-clf.score(train_predictor_data,train_target_data)
 
-num_test = len(test)
-#Load test data and make predictions
-test_data = test.iloc[:num_test,:].values
-preds = clf.predict(test_data)
-
-#Predictions out to file
-out = pd.DataFrame(zip(range(1,len(preds)+1),preds),columns=('ImageId','Label'))
-out.to_csv('res.lda.csv',index=False)
+#Save model
+barrel = open(sys.argv[1], 'wb')
+pickle.dump(clf, barrel)
+barrel.close()
 
 
-#Display images and predictions for a few random obvs
-num_row, num_col  = 5, 5
-sample = np.random.choice(range(0,len(test_data)), num_row*num_col, replace=False)
-for idx, sample_idx in enumerate(sample):
-    plt.subplot(num_row, num_col, idx + 1)
-    plt.axis('off')
-    image = test_data[sample_idx].reshape(28,28)
-    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-    plt.title('%d' % preds[sample_idx])
 
-plt.show()
+
