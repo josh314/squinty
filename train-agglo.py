ceffb3aabfae84a3193d9d38103970f58985bf0f
Joshua Davis <josh314@gmail.com>
2015-08-25 16:04:39 -0400

    Changed clustering workflow. agglo-trainer spits out an agglo.p object as well as reduced training data. reduce the test data elsewhere in make-predictions.py. make-predictions-basic.py makes predictions for simple models which need no test data processing or extraction of best estimator

--- train-agglo.py
+++ train-agglo.py
@@ -1,31 +1,30 @@
 ################################################################
 # Cluster reduction of pixel data
 #
-# Usage: python2 cluster-dimensional-reduction.py <infile> [options]
+# Usage: python2 train-agglo.py <training data> [options]
 #
-# 
-
-
+# Builds the agglo (Agglommerative clusting object) which
+# reduces problem dimension
 
 #Parse command line options and arguments
 #Do this before other imports so incorrect usage message appears quickly
 import optparse
 parser = optparse.OptionParser()
 
-parser.add_option("-t", action="store_true", dest="t")
 parser.add_option("-o", action="store", type="string", dest="o")
 parser.add_option("-n", action="store", type="int", dest="n")
-parser.set_defaults(t=False, o="out.csv", n=28*28)
+parser.add_option("-m", action="store", type="string", dest="m")
+parser.set_defaults(o="out.csv", n=14*14, m="agglo.p")
 opts, args = parser.parse_args()
 
 if(len(args) < 1):
-    print "Usage: python2 cluster-dimensional-reduction.py <infile> [options]"
+    print "Usage: python2 train-agglo.py <training data> [options]"
     raise SystemExit(1)
 
 infile = args[0]
 outfile = opts.o
-is_training_data = opts.t
 n_clusters = opts.n
+model_fn = opts.m
 
 #Other imports
 import pickle
@@ -33,18 +32,14 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.image import grid_to_graph
 import sklearn.cluster as cluster
-import matplotlib.pyplot as plt
+
 
 #Full resolution images 
 fullres = pd.read_csv(infile,header=0)
 
 num_images = len(fullres)
-#If a training set, separate out label data and convert to numpy array
-if(is_training_data):
-    image_data = fullres.iloc[ :num_images, 1:].values
-    labels = fullres.iloc[ :num_images, 0].values
-else:
-    image_data = fullres.iloc[ :num_images, :].values
+image_data = fullres.iloc[ :num_images, 1:].values
+labels = fullres.iloc[ :num_images, 0].values
     
 #Unflatten to proper 2D shape and extract the connectivity graph
 images = image_data.reshape(num_images,28,28)
@@ -52,20 +47,25 @@ connectivity = grid_to_graph(*images[0].shape)
 
 #Do the clustering in feature space
 agglo = cluster.FeatureAgglomeration(connectivity=connectivity, n_clusters=n_clusters)
-agglo.fit(image_data) 
+agglo.fit(image_data)
+
+# Save agglo for future use in workflow.
+# Must use this same agglo for test data
+barrel=open(model_fn,'wb')
+pickle.dump(agglo,barrel)
+barrel.close()
 
 #Transform the original data into reduced feature space
 image_data_reduced = agglo.transform(image_data)
 
 #Reduced data out to file
 out = pd.DataFrame(image_data_reduced)
-if(is_training_data):
-    out.insert(0, 'label', labels)#Add back the target labels 
+out.insert(0, 'label', labels)#Add back the target labels 
 out.to_csv(outfile,index=False)
 
 #
 #Display some approx images to get a sense of what has been stripped out
-#
+# import matplotlib.pyplot as plt
 #Go back to real space from eigenpixel space
 # image_data_approx = agglo.inverse_transform(image_data_reduced)
 # num_row, num_col  = 5, 5
