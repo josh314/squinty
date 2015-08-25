641b6c12cc743a5ce5476f84e478f0c11b39d6bd
Joshua Davis <josh314@gmail.com>
2015-08-25 10:54:38 -0400

    Finished converting svm.py to use command line arguments instead of hard coded filenames. Takes in training data and spits out a pickled trained model.

--- svm.py
+++ svm.py
@@ -1,28 +1,26 @@
 import sys
-if len(sys.argv) != 2:
-    print "Usage: python svm.py <pickled model output>"
+if len(sys.argv) != 3:
+    print "Usage: python svm.py <training data> <pickled model output>"
     raise SystemExit(1)
-import os
 import pickle
 import pandas as pd
 import numpy as np
 from sklearn import svm
 
-os.chdir('/Users/josh/dev/kaggle/digit-recognizer')
-train = pd.read_csv('data/raw/train.csv',header=0)
-test = pd.read_csv('data/raw/test.csv',header=0)
+train = pd.read_csv(sys.argv[1],header=0)
 
 num_fit = len(train)
 #Separate out label data and convert to numpy array
 train_predictor_data = train.iloc[ :num_fit, 1:].values
 train_target_data = train.iloc[ :num_fit, 0].values
 
-#Train dat sucker
+#Estimator constructor 
 clf = svm.SVC(gamma=1e-8,verbose=True,shrinking=False)
+#Training montage
 clf.fit(train_predictor_data, train_target_data)
 
 #Save model
-barrel = open(sys.argv[1], 'wb')
+barrel = open(sys.argv[2], 'wb')
 pickle.dump(clf, barrel)
 barrel.close()
 
