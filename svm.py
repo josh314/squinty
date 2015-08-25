ab8fbadd6278628715c772990e3a2c41100e066d
Joshua Davis <josh314@gmail.com>
2015-08-25 11:00:53 -0400

    cleanup

--- svm.py
+++ svm.py
@@ -2,14 +2,17 @@ import sys
 if len(sys.argv) != 3:
     print "Usage: python svm.py <training data> <pickled model output>"
     raise SystemExit(1)
+
+train_fn, model_fn = sys.argv[1], sys.argv[2]
+
 import pickle
 import pandas as pd
 import numpy as np
 from sklearn import svm
 
-train = pd.read_csv(sys.argv[1],header=0)
+train = pd.read_csv(train_fn,header=0)
 
-num_fit = len(train)
+num_fit = 100#len(train)
 #Separate out label data and convert to numpy array
 train_predictor_data = train.iloc[ :num_fit, 1:].values
 train_target_data = train.iloc[ :num_fit, 0].values
@@ -20,7 +23,7 @@ clf = svm.SVC(gamma=1e-8,verbose=True,shrinking=False)
 clf.fit(train_predictor_data, train_target_data)
 
 #Save model
-barrel = open(sys.argv[2], 'wb')
+barrel = open(model_fn, 'wb')
 pickle.dump(clf, barrel)
 barrel.close()
 
