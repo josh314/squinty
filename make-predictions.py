2ecdde800e3de3bc893365566b6c857816514802
Joshua Davis <josh314@gmail.com>
2015-09-02 13:06:00 -0400

    latest

--- make-predictions.py
+++ make-predictions.py
@@ -17,18 +17,21 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Deserialize reducer object for pre-processing test data
-barrel = open(reducer_fn, 'rb')
-reducer = pickle.load(barrel)
-barrel.close()
+
 
 #Load test data and make predictions
 test = pd.read_csv(infile,header=0)
 num_test = len(test)
 test_data = test.iloc[:num_test,:].values
 
-test_data_reduced = reducer.transform(test_data)
-
+test_data_reduced = test_data
+reducer = None
+if(reducer_fn!=""):
+    #Deserialize reducer object for pre-processing test data
+    barrel = open(reducer_fn, 'rb')
+    reducer = pickle.load(barrel)
+    barrel.close()
+    test_data_reduced = reducer.transform(test_data)    
 
 #Deserialize model
 barrel = open(model_fn, 'rb')
@@ -42,16 +45,16 @@ preds = clf.predict(test_data_reduced)
 out = pd.DataFrame(zip(range(1,len(preds)+1),preds),columns=('ImageId','Label'))
 out.to_csv(outfile,index=False)
 
-
-#Display images and predictions for a few random obvs
-num_row, num_col  = 5, 5
-image_data_approx = reducer.inverse_transform(test_data_reduced)
-sample = np.random.choice(range(0,num_test), num_row*num_col, replace=False)
-for idx, sample_idx in enumerate(sample):
-    plt.subplot(num_row, num_col, idx + 1)
-    plt.axis('off')
-    image = image_data_approx[sample_idx].reshape(28,28)
-    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-    plt.title('%d' % preds[sample_idx])
-
-plt.show()
+if reducer:
+    #Display images and predictions for a few random obvs
+    image_data_approx = reducer.inverse_transform(test_data_reduced)
+    num_row, num_col  = 5, 5
+    sample = np.random.choice(range(0,num_test), num_row*num_col, replace=False)
+    for idx, sample_idx in enumerate(sample):
+        plt.subplot(num_row, num_col, idx + 1)
+        plt.axis('off')
+        image = image_data_approx[sample_idx].reshape(28,28)
+        plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+        plt.title('%d' % preds[sample_idx])
+    
+    plt.show()
--- render-images.py
+++ render-images.py
@@ -3,7 +3,8 @@ parser = optparse.OptionParser()
 
 parser.add_option("-t", action="store_true", dest="t")
 parser.add_option("-n", action="store", type="int", dest="n")
-parser.set_defaults(t=False, n=0)
+parser.add_option("-r", action="store", type="string", dest="r")
+parser.set_defaults(t=False, n=0, r="")
 opts, args = parser.parse_args()
 
 if(len(args) < 1):
@@ -13,6 +14,7 @@ if(len(args) < 1):
 infile = args[0]
 is_training_set = opts.t
 num_display = opts.n
+reducer_fn = opts.r
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -25,11 +27,22 @@ label = ""
 if(is_training_set):
     label = train.loc[num_display,'label']
 
+
 #Get the pixel data for one of the images.
 #column 1 is the training label if applicable, so skip that 
 #Expand the flat array into 2-dimensional data. Images are 28x28 pixels
 skip = 1 if is_training_set else 0
 image_flat = train.iloc[num_display,skip:].values
+
+reducer = None
+if(reducer_fn!=""):
+    #Deserialize reducer object for pre-processing test data
+    barrel = open(reducer_fn, 'rb')
+    reducer = pickle.load(barrel)
+    barrel.close()
+    image_reduced = reducer.transform(image_flat)    
+    image_flat = reducer.inverse_transform(image_reduced)
+
 image = image_flat.reshape(28,28)
 
 #Add image to plot in grayscale
